d84cdd15e56e44b4a704a4294cc233f53d350ba8
babakdorani <babak.dorani@gmail.com>
2021-10-02 08:40:32 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src('index.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+  return src("scss/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(["scss/**/*.scss"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
