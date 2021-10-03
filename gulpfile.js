ac6c584656191db5ea9a6e63c96642dd5cc883da
babakdorani <babak.dorani@gmail.com>
2021-10-03 08:18:04 +0300

    Navarbar Purge added and it watch all the html files inside root. please check gulpfile.js
    grid added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("scss/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("scss/**/*.scss")
+    .pipe(sass())
+
+    .pipe(
+      purgecss({
+        content: ["*.html"],
+      })
+    )
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["scss/**/*.scss"], buildStyles);
+  watch(["scss/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
